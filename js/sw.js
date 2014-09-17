cba9ac0af0edb7d985ed3d56eed08734792f6e88
Balázs Suhajda <balazs@onereason.eu>
2014-09-17 14:24:31 +0200

    moar logging

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,15 @@
+this.addEventListener('install', function(event) {
+	console.log(event);
+});
+
+this.addEventListener('activate', function(event) {
+	console.log(event);
+});
+
 this.addEventListener('fetch', function(event) {
 	console.log(event);
-	event.respondWith(
+/*	event.respondWith(
 		new Response('This came from the service worker!')
 	);
+*/
 });
