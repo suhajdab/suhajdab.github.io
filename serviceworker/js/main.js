57f1be85e026cf0eccccf613dcefe2e92b327541
Balázs Suhajda <balazs@onereason.eu>
2014-09-18 17:45:36 +0200

    never change stuff right before demo, duh

--- serviceworker/js/main.js
+++ serviceworker/js/main.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('js/sw.js', {
 		scope: '/serviceworker/'
-	}).then(function(sw) {
+	}).then(function( sw ) {
 		console.log("sw registration worked!");
-	}).catch(function() {
-		console.log("registration failed :(");
+	}).catch(function( err ) {
+		console.log("registration failed :(", err );
 	});
 }
