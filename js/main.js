19a727a1c6ae86a72c639fdd0cc5262e9da3dee4
Balázs Suhajda <balazs@onereason.eu>
2014-09-18 17:37:44 +0200

    Re-enable network kidnapping

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 if ( 'serviceWorker' in navigator ) {
-	navigator.serviceWorker.register( '/js/sw.js', {
+	navigator.serviceWorker.register('/js/sw.js', {
 		scope: '/serviceworker/'
-	}).then( function( sw ) {
-		console.log( "sw registration worked!" );
-	}).catch( function() {
-		console.log( "registration failed :(" );
+	}).then(function(sw) {
+		console.log("sw registration worked!");
+	}).catch(function() {
+		console.log("registration failed :(");
 	});
 }
